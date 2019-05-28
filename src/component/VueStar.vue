a22dd464ff34c42c25bbc0f86ef7b0f6375c996b
kevin <kevin@t9ing.com>
2019-05-28 22:24:01 +0800

    add new props: active, disabled add event toggle no longer out of normal flow

--- src/component/VueStar.vue
+++ src/component/VueStar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="VueStar"> 
+  <div class="VueStar">
     <div class="VueStar__ground">
-      <div class="VueStar__icon" @click="toggle" :class="AnimateClass" :style='{color:ColorValue}'>
+      <div class="VueStar__icon" @click="toggle" :class="AnimateClass" :style="{color:ColorValue}">
         <slot name="icon"></slot>
       </div>
-      <div class="VueStar__decoration" :class="{ 'VueStar__decoration--active':active}"></div>
+      <div class="VueStar__decoration" :class="{'VueStar__decoration--active': isActive}"></div>
     </div>
   </div>
 </template>
@@ -14,40 +14,49 @@ import { isColors } from '../uitls/colorRE.js'
 export default {
   name: 'VueStar',
   props: {
-    animate: String,
-    color: String
-  },
-  methods: {
-    toggle () {
-      this.active = !this.active
-      this.toggleAnimate = !this.toggleAnimate
-      this.toggleColor = !this.toggleColor
+    animate: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: ''
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
     return {
-      active: false,
-      toggleAnimate: false,
-      toggleColor: false
+      isActive: this.active
     }
   },
   computed: {
     AnimateClass () {
-      return this.toggleAnimate ? this.animate : ''
+      return this.isActive ? this.animate : ''
     },
     ColorValue () {
-      return this.toggleColor ? this.color : ''
+      return this.isActive ? this.color : ''
     }
   },
   mounted () {
     if (this.color) {
-      if (isColors(this.color)) {
-        return
-      } else {
+      if (!isColors(this.color)) {
         console.error('this color must be hexcolor or rgbcolor  ---VueStar')
       }
-    } else {
-      return
+    }
+  },
+  methods: {
+    toggle () {
+      if (!this.disabled) {
+        this.isActive = !this.isActive
+        this.$emit('toggle', this.isActive)
+      }
     }
   }
 }
